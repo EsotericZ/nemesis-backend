c06055956b26e1483b10c5d0c8a08ce7c749cf01
EsotericZ <cjsand03@gmail.com>
2023-08-07 04:45:36 -0600

    finish login:

--- controllers/portalController.js
+++ controllers/portalController.js
@@ -3,8 +3,8 @@ const bcrypt = require('bcrypt');
 const jwt = require("jsonwebtoken");
 
 const login = async (req, res) => {
+    const cookies = req.cookies;
     const { email, password } = req.body; 
-
     const userFound = await User.findOne({ email: email }).exec();
     if (!userFound) return res.sendStatus(401);
 
@@ -19,21 +19,42 @@ const login = async (req, res) => {
                 }
             },
             process.env.ACCESS_TOKEN_SECRET,
-            { expiresIn: '10s' }
+            { expiresIn: '10m' }
         );
-        const refreshToken = jwt.sign(
+        const newRefreshToken = jwt.sign(
             { "email": userFound.email, },
             process.env.REFRESH_TOKEN_SECRET,
             { expiresIn: '1d' }
         );
-        userFound.refreshToken = refreshToken;
-        await userFound.save();
-        res.cookie('jwt', refreshToken, { httpOnly: true, secure: true, sameSite: 'None', maxAge: 24 * 60 * 60 * 1000 });
-        res.json({ roles, accessToken });
+
+        let newRefreshTokenArray =
+            !cookies?.jwt
+                ? userFound.refreshToken
+                : userFound.refreshToken.filter(rt => rt !== cookies.jwt);
+
+        if (cookies?.jwt) {
+            const refreshToken = cookies.jwt;
+            const tokenFound = await User.findOne({ refreshToken }).exec();
+
+            if (!tokenFound) {
+                newRefreshTokenArray = [];
+            }
+
+            res.clearCookie('jwt', { httpOnly: true, sameSite: 'None', secure: true });
+        }
+        
+        userFound.refreshToken = [...newRefreshTokenArray, newRefreshToken];
+        const result = await userFound.save();
+        console.log(result);
+
+        res.cookie('jwt', newRefreshToken, { httpOnly: true, secure: true, sameSite: 'None', maxAge: 24 * 60 * 60 * 1000 });
+        res.json({ accessToken });
     } else {
         res.sendStatus(401);
     }
 }
+
+
 
 const logout = async (req, res) => {
     const cookies = req.cookies;
@@ -46,24 +67,45 @@ const logout = async (req, res) => {
         return res.sendStatus(204);
     }
 
-    userFound.refreshToken = '';
-    await userFound.save();
+    userFound.refreshToken = userFound.refreshToken.filter(rt => rt !== refreshToken);
+    const result = await userFound.save();
+    console.log(result);
 
     res.clearCookie('jwt', { httpOnly: true, secure: true, sameSite: 'None'});
     res.sendStatus(204);
 }
 
+
+
 const refreshToken = async (req, res) => {
     const cookies = req.cookies;
     if (!cookies?.jwt) return res.sendStatus(401);
     const refreshToken = cookies.jwt;
-
+    res.clearCookie('jwt', { httpOnly: true, secure: true, sameSite: 'None'});
     const userFound = await User.findOne({ refreshToken }).exec();
-    if (!userFound) return res.sendStatus(403);
+    if (!userFound) {
+        jwt.verify(
+            refreshToken,
+            process.env.REFRESH_TOKEN_SECRET,
+            async (err, decoded) => {
+                if (err) return res.sendStatus(403);
+                const hackedUser = await User.findOne({ email: decoded.email }).exec();
+                hackedUser.refreshToken = [];
+                await hackedUser.save();
+            }
+        )
+        return res.sendStatus(403);
+    }
+
+    const newRefreshTokenArray = userFound.refreshToken.filter(rt => rt !== refreshToken);
     jwt.verify(
         refreshToken,
         process.env.REFRESH_TOKEN_SECRET,
-        (err, decoded) => {
+        async (err, decoded) => {
+            if (err) {
+                userFound.refreshToken = [...newRefreshTokenArray];
+                await userFound.save();
+            }
             if (err || userFound.username !== decoded.username) return res.sendStatus(403);
             const roles = Object.values(userFound.roles);
             const accessToken = jwt.sign(
@@ -76,10 +118,22 @@ const refreshToken = async (req, res) => {
                 process.env.ACCESS_TOKEN_SECRET,
                 { expiresIn: '10s' }
             );
-            res.json({ roles, accessToken })
+
+            const newRefreshToken = jwt.sign(
+                { "email": userFound.email, },
+                process.env.REFRESH_TOKEN_SECRET,
+                { expiresIn: '1d' }
+            );
+
+            userFound.refreshToken = [...newRefreshTokenArray, newRefreshToken];
+            await userFound.save();
+            res.cookie('jwt', newRefreshToken, { httpOnly: true, secure: true, sameSite: 'None', maxAge: 24 * 60 * 60 * 1000 });
+            res.json({ accessToken })
         }
     );
 }
+
+
 
 const register = async (req, res) => {
     const { email, password } = req.body;
