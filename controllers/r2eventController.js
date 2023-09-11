fe737a7d4d0929fec4022267902191cc88bc1f3f
EsotericZ <cjsand03@gmail.com>
2023-09-10 18:22:53 -0600

    backend r2

--- controllers/r2eventController.js
+++ controllers/r2eventController.js
@@ -1,12 +1,18 @@
-const R2Event = require('../models/Event');
+const R2Event = require('../models/R2Event');
 
 const createR2Event = async (req, res) => {
-    const newR2Event = req.body.newR2Event;
+    // const newR2Event = req.body.newR2Event;
+    // console.log(newR2Event);
+    let event = {
+        eventName: 'test'
+    }
 
     try {
-        await R2Event.create(newR2Event)
+        await R2Event.create(event)
+        console.log('event created')
         res.status(201).json({ 'success': 'Event Created' })
     } catch (err) {
+        console.log('event failed')
         res.status(500).json({ 'message': err.message });
     }
 }
--- models/index.js
+++ models/index.js
@@ -1,5 +1,7 @@
+const R2Event = require('./R2Event');
 const User = require('./User');
 
 module.exports = {
+    R2Event,
     User,
 }
