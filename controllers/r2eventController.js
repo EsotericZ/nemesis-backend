72ee7620441a0bceaffe00d6a820f854d5d98a2e
EsotericZ <cjsand03@gmail.com>
2023-09-11 18:34:24 -0600

    finalize r2 events

--- controllers/r2eventController.js
+++ controllers/r2eventController.js
@@ -1,18 +1,14 @@
 const R2Event = require('../models/R2Event');
 
 const createR2Event = async (req, res) => {
-    // const newR2Event = req.body.newR2Event;
-    // console.log(newR2Event);
-    let event = {
-        eventName: 'test'
-    }
+    const newR2Event = req.body.newR2Event;
+    newR2Event.startDate = req.body.startDate;
+    newR2Event.endDate = req.body.endDate;
 
     try {
-        await R2Event.create(event)
-        console.log('event created')
+        await R2Event.create(newR2Event)
         res.status(201).json({ 'success': 'Event Created' })
     } catch (err) {
-        console.log('event failed')
         res.status(500).json({ 'message': err.message });
     }
 }
--- models/R2Event.js
+++ models/R2Event.js
@@ -6,36 +6,36 @@ const r2eventSchema = new Schema({
         type: String,
         required: true,
     },
-    // location: {
-    //     type: String,
-    //     required: true,
-    // },
-    // club: {
-    //     type: String,
-    //     required: true,
-    // },
-    // startDate: {
-    //     type: Date,
-    // },
-    // endDate: {
-    //     type: Date,
-    // },
-    // director: {
-    //     type: String,
-    // },
-    // status: {
-    //     type: String,
-    //     default: 'future',
-    // },
-    // image: {
-    //     type: String,
-    // },
-    // description: {
-    //     type: String,
-    // },
-    // eventURL: {
-    //     type: String,
-    // }
+    location: {
+        type: String,
+        required: true,
+    },
+    club: {
+        type: String,
+        required: true,
+    },
+    startDate: {
+        type: Date,
+    },
+    endDate: {
+        type: Date,
+    },
+    director: {
+        type: String,
+    },
+    status: {
+        type: String,
+        default: 'future',
+    },
+    image: {
+        type: String,
+    },
+    description: {
+        type: String,
+    },
+    eventURL: {
+        type: String,
+    }
 });
 
 module.exports = mongoose.model('R2Event', r2eventSchema);
